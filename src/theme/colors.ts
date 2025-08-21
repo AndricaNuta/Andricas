514d79760109e96bebc93a08e80b957accbadeb1
anuta <anuta@softserveinc.com>
2025-08-21 15:27:52 +0300

    adding innitial theme files

---
+++ src/theme/colors.ts
@@ -0,0 +1,18 @@
+export const lightColors = {
+    background: '#FFFFFF',
+    text: '#000000',
+    primary: '#6C63FF',
+    card: '#F5F5F5',
+    border: '#E0E0E0',
+    error: '#FF4C4C',
+  };
+  
+  export const darkColors = {
+    background: '#000000',
+    text: '#FFFFFF',
+    primary: '#6C63FF',
+    card: '#1C1C1C',
+    border: '#333333',
+    error: '#FF6B6B',
+  };
+  
